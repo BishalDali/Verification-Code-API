ff72f8e192d384b7b12ac407c489ff4422fa350e
Milan Roka <84458919+BishalDali@users.noreply.github.com>
2023-06-01 18:04:18 +0545

    Simplified code

--- src/controllers/verification-code.controller.ts
+++ src/controllers/verification-code.controller.ts
@@ -1,20 +1,17 @@
-import {Response, Request} from 'express'
-import { verifyCodeService } from '../services/verification-code.service'
-import { VerificationCodeDTO } from '../dto/verification-code.dto'
+import { Response, Request } from "express";
+import { isCodeValid } from "../services/verification-code.service";
+import { VerificationCodeDTO } from "../dto/verification-code.dto";
 
-export const verifyCode = (req: Request, res : Response) => {
-    const {code} : VerificationCodeDTO = req.body
-    try {
-        const isVerified = verifyCodeService({code})
-        if(isVerified){
-            res.status(200).json({
-                message : "Verification Successful"
-            })
-        }
-        
-    } catch (error) {
-        res.status(400).json({
-            message: "Invalid Code"
-        })
-    }
-}
+export const verifyCode = (req: Request, res: Response) => {
+  const { code }: VerificationCodeDTO = req.body;
+  const isVerified = isCodeValid({ code });
+  if (isVerified) {
+    return res.status(200).json({
+      message: "Verification Successful",
+    });
+  } else {
+    return res.status(400).json({
+      message: "Invalid Code",
+    });
+  }
+};
